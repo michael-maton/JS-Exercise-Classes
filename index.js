e8324d3cc0a594217e6b31fb4a8434e8ffb9c82f
Michael Maton <mjmaton6@gmail.com>
2020-10-15 15:17:08 -0500

    task 6 complete

--- index.js
+++ index.js
@@ -211,8 +211,18 @@ console.log(mike.listSubjects())
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(obj){
+    super(obj);
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+  debugsCode(studenObj, subject){
+    return `${this.name} debugs ${studenObj.name}'s code on ${subject}`;
+  }
 }
 
 /*
